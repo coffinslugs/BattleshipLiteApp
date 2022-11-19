5b0a503c97aeff96b89c89c063b58bac9147c84a
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2022-11-19 14:54:34 -0700

    Add files via upload

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,74 @@
+﻿using BattleShipLiteLibrary;
+using BattleShipLiteLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipLite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+            PlayerInfoModel player1 = CreatePlayer("Player 1");
+            PlayerInfoModel player2 = CreatePlayer("Player 2");
+
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Battleship Lite!");
+            Console.WriteLine("By birdcoffee");
+            Console.WriteLine();
+        }
+
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            Console.WriteLine($"Player information for { playerTitle }.");
+
+            // Ask user for their name
+            output.PlayerName = AskForPlayersName();
+            
+            // Load up grid
+            GameLogic.InitialiazeGrid(output);
+
+            // Ask user for their ship placement
+            PlaceShips(output);
+
+            // Clear screen
+            Console.Clear();
+
+            return output;
+        }
+
+        private static string AskForPlayersName()
+        {
+            Console.Write("What is your name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void PlaceShips(PlayerInfoModel model)
+        {
+            do
+            {
+                Console.Write($"Where do you want to place ship number {model.ShipLocation.Count + 1}: ");
+                string location = Console.ReadLine();
+
+                bool isValidLocation = GameLogic.PlaceShip(model, location);
+
+                if (isValidLocation == false)
+                {
+                    Console.WriteLine("That was not a valid location. Please try again.");
+                }
+
+            } while (model.ShipLocation.Count < 5);
+        }
+    }
+}
